d82b222626c3e66b43c7d126dd123e5b97f85150
Maan Shamoon <Maan Shamoon>
2017-03-02 14:49:00 -0800

    fixed hidden menu

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
+	//$(".mobile-header").hide();
 	$(".mobile-nav-bar").hide();
+	
 
 	$(".burger-menu").click(function(){
 		$(".mobile-nav-bar").slideToggle("slow");
