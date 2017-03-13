858b1ef6d6428ce8bc4114a89a74814b065c3d5d
Maan Shamoon <Maan Shamoon>
2017-03-12 19:56:45 -0700

    catering nav bar fixed

--- static/js/catering.js
+++ static/js/catering.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
         $(".mobile-header").show();
         $(".mobile-nav-bar").hide();
