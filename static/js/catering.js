e1e22ed55d44c97352f674ab9b276e06cd23ceb5
Maan Shamoon <Maan Shamoon>
2017-03-15 20:04:00 -0700

    all final changes made. fixed the margins and weird bugs.

--- static/js/catering.js
+++ static/js/catering.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
 
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        $(".mobile-header").show();
-        $(".mobile-nav-bar").hide();
-    }
 
     $(".burger-menu").click(function(){
         $(".mobile-nav-bar").slideToggle(500);
+    });
+
+    $(".click-here").click(function(e){
+
+        var nav_bar_height = 0;
+
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+            nav_bar_height =  $(".mobile-header").height();
+        }else{
+            nav_bar_height =  $(".nav-bar").height();
+        }
+
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $(".scroll-to").offset().top - nav_bar_height - 20
+        }, 1000);
+        
     });
 
     $.getJSON("js/catering.json", function(data){
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        $(".mobile-header").show();
-        $(".mobile-nav-bar").hide();
-    }
-
     $(".burger-menu").click(function(){
         $(".mobile-nav-bar").slideToggle(500);
     });
